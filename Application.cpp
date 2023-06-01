0e8ba52da003ac06c43d83320daa1a35874384ff
Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
2023-06-02 02:22:41 +0530

    textured

--- Application.cpp
+++ Application.cpp
@@ -2,6 +2,8 @@
 #include <GLFW/glfw3.h>
 
 #include <../Shader/shader.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <../ImageLoader/stb_image.h>
 
 #include <iostream>
 
@@ -42,26 +44,84 @@ int main()
 	Shader ourShader("shader.vert", "shader.frag");
 
 	float vertices[] = {
-		//Vertices                 Colors
-		0.5f, -0.5f, 0.0f,		 1.0f, 0.0f, 0.0f,
-	   -0.5f, -0.5f, 0.0f,		 0.0f, 1.0f, 0.0f,
-		0.0f,  0.5f, 0.0f,		 0.0f, 0.0f, 1.0f
+		//Vertices                 Colors				  Textures
+		0.5f,  0.5f, 0.0f,		 1.0f, 0.0f, 0.0f,		1.0f, 1.0f,
+	    0.5f, -0.5f, 0.0f,		 0.0f, 1.0f, 0.0f,		1.0f, 0.0f,
+	   -0.5f, -0.5f, 0.0f,		 0.0f, 0.0f, 1.0f,		0.0f, 0.0f,
+	   -0.5f,  0.5f, 0.0f,       1.0f, 1.0f, 0.0f,		0.0f, 1.0f
 	};
 
-	unsigned int VBO, VAO;
+	unsigned indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	//Telling OpenGl how to interpret our data
 	//Position Attribute Pointer
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	//Color Attribute Pointer
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	//Texture Attribute Pointer
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	unsigned int texture_1, texture_2;
+	glGenTextures(1, &texture_1);
+	glBindTexture(GL_TEXTURE_2D, texture_1);
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int height, width, nrChannels;
+	unsigned char* data = stbi_load("textures/container.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	glGenTextures(1, &texture_2);
+	stbi_set_flip_vertically_on_load(true);
+	glBindTexture(GL_TEXTURE_2D, texture_2);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	data = stbi_load("textures/awesomeface.png", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	ourShader.use();
+	glUniform1i(glGetUniformLocation(ourShader.ID, "ourTexture_1"), 0);
+	ourShader.setInt("ourTexture_2", 1);
 
 	while(!glfwWindowShouldClose(window))
 	{
@@ -72,9 +132,20 @@ int main()
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+
+		float time = glfwGetTime();
+		float alpha = sin(time)*0.5f + 0.5f;
+		ourShader.setFloat("alpha", alpha);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture_1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture_2);
+
+
 		ourShader.use();
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		//Check and call events and swap buffers
 		glfwSwapBuffers(window);
--- shader.frag
+++ shader.frag
@@ -1,7 +1,15 @@
 #version 330 core
+
 out vec4 FragColor;
+
 in vec3 ourColor;
+in vec2 texCoord;
+
+uniform sampler2D ourTexture_1;
+uniform sampler2D ourTexture_2;
+uniform float alpha;
+
 void main()
 {
-   FragColor = vec4(ourColor, 1.0f);
-}
+   FragColor = mix(texture(ourTexture_1, texCoord), texture(ourTexture_2, texCoord), alpha);
+}  
